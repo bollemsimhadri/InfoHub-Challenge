372859a7ead7af71a29d5b8c2308f784e4052881
simhadri <bollemsimhadri@gmail.com>
2025-11-06 15:11:54 +0530

    update in apiPath.js

--- client/src/components/data/apiPath.js
+++ client/src/components/data/apiPath.js
@@ -1,2 +1,5 @@
-export const API_URL ='http://localhost:3001'
+//export const API_URL ='http://localhost:3001'
+
+// data/apiPath.js
+export const API_URL = 'https://infohub-challenge-1-iag2.onrender.com';
 
